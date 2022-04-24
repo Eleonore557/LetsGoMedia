3806bb944373bd505733eaa37da19f4a3614d2bb
Eléonore <eleonore0528@gmail.com>
2022-04-25 00:22:10 +0200

    css

--- src/components/view-tag/index.js
+++ src/components/view-tag/index.js
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 const ViewTag = styled.View`
   position: relative;
-  top: -90px;
-  margin-bottom: 80px;
+  top: -30px;
+  margin-bottom: 110px;
   margin-left: auto;
   margin-right: auto;
 `
